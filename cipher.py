3d3fdab84eb89400a772cda6ae37651c7aad6acc
Celestino Martinez <Celestino.Martinez01@gmail.com>
2022-02-17 17:47:40 -0500

    Create cipher.py

---
+++ cipher.py
@@ -0,0 +1,24 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(text, shift, cypher_direction):
+  word = ""
+  if cypher_direction == "decode":
+      shift *= -1
+
+  for char in text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift
+      word += alphabet[new_position]
+    else:
+      word+=char
+  print("The " + cypher_direction + " text is " + word)
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+shift %= 26
+
+caesar(text, shift, direction)
